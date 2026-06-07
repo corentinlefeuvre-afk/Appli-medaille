c24a3c2c3b8211e1278a70b5a4ad87d267417381
Corentin Lefeuvre <corentin.lefeuvre@FNPC.local>
2026-06-07 23:42:20 +0200

    Premier envoi officiel de l'appli medaille

---
+++ deploy.bat
@@ -0,0 +1,8 @@
+@echo off
+set PATH=%PATH%;C:\Users\corentin.lefeuvre\Downloads\Logiciels\node-v24.14.1-win-x64\node-v24.14.1-win-x64
+git add .
+git commit -m "Update"
+git push
+echo.
+echo Deploiement envoye !
+pause
