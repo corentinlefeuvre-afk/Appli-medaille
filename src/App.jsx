b2bf5df429c387f9e849727197a92b773000cf31
Corentin Lefeuvre <corentin.lefeuvre@FNPC.local>
2026-09-03 17:13:12 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ class ErrorBoundary extends React.Component {
 export { ErrorBoundary };
 
 const APP_TITLE   = "Demande Médaille FNPC";
-const APP_VERSION = "1.6.16";
+const APP_VERSION = "1.6.17";
 const USE_SUPABASE = true;
 
 // ── PrestaShop Webservice ────────────────────────────────────────────────────
